4eb855b9e2f0a4836bf339381a604b5b8c5441c6
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-11-17 17:14:03 +0530

    commit

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -7,27 +7,36 @@
     <link href="{{asset('frontend/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg bg-default border">
-        <div class="container-fluid px-4">
+    <nav class="navbar navbar-expand-lg bg-default border bg-body-tertiary">
+        <div class="container">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                 <a class="navbar-brand" href="#">
-                    <img src="{{asset('frontend/images/logo.png')}}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                    <img src="{{asset('frontend/images/logo.png')}}" alt="" height="70" class="d-inline-block align-text-top">
                 </a>
-                <ul class="navbar-nav me-0 mb-2 mb-lg-0">
+                <ul class="navbar-nav mb-2 mb-lg-0 me-auto">
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="#">Home</a>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Link
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+              
                 </ul>
-                <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
-    </nav>      
-    <script src="{{asset('frontend/bootstrap/js/bootstrap.bundle.min.js')}}" crossorigin="anonymous"></script>
+    </nav>
+    <script src="{{asset('frontend/js/jquery.min.js')}}"></script>   
+    <script src="{{asset('frontend/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   </body>
 </html>
