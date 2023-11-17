37a6aa622b2a9567645504c57056d7eb2e3be0e5
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-11-17 17:49:34 +0530

    commit

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -7,36 +7,39 @@
     <link href="{{asset('frontend/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg bg-default border bg-body-tertiary">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{asset('frontend/images/logo.png')}}" alt="" height="70" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="#">
-                    <img src="{{asset('frontend/images/logo.png')}}" alt="" height="70" class="d-inline-block align-text-top">
-                </a>
-                <ul class="navbar-nav mb-2 mb-lg-0 me-auto">
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll float-end" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Link
-                        </a>
-                        <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><hr class="dropdown-divider"></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                      </li>
-              
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Industry Notifications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Join Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
                 </ul>
             </div>
-        </div>
-    </nav>
-    <script src="{{asset('frontend/js/jquery.min.js')}}"></script>   
-    <script src="{{asset('frontend/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+            </div>
+        </nav>        
+        <script src="{{asset('frontend/js/jquery.min.js')}}"></script>   
+        <script src="{{asset('frontend/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   </body>
 </html>
