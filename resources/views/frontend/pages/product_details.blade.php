e0302a1f93943365a6ce83ce5573cb32f7b56517
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-11-23 16:52:44 +0530

    Update product_details.blade.php

--- resources/views/frontend/pages/product_details.blade.php
+++ resources/views/frontend/pages/product_details.blade.php
@@ -20,22 +20,43 @@
     <div class="uk-container">
         <div uk-grid>
             <div class="uk-width-1-4@m">
+                <span class="uk-margin-small-left uk-heading-bullet uk-text-bold">{{$product->product_name}}</span>
                 <ul class="uk-tab-left" uk-tab="connect: #component-tab-left; animation: uk-animation-fade">
                     <li>
-                        <a href="#{{$section->service_section_slug}}">{{$section->service_section_name}}</a>
+                        <a href="#">{{'Overview'}}</a>
                     </li>
                     @if($sections)
                     @foreach($sections as $section)
                         <li>
-                            <a href="#{{$section->service_section_slug}}">{{$section->service_section_name}}</a>
+                            <a href="#{{$section->product_section_slug}}">{{$section->product_section_name}}</a>
                         </li>
-                        @if($loop->first)
-                            @if($service->service_product_show === 1) 
-                            <li class="nav-item">
-                                <a href="#{{'mandatory-product-list'}}">Mandatory Product List</a>
-                            </li>
-                            @endif
-                        @endif
+                    @endforeach
+                    @endif
+                    <li>
+                        <a href="#">{{'Download Brochure'}}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="uk-width-expand@m">
+                <ul id="component-tab-left" class="uk-switcher">
+                    <li>
+                        <div class="uk-container ps-tab-header">
+                            <span>Overview<span>
+                        </div>
+                        <div class="uk-container ps-tab-content uk-height-large">
+                            {!! $product->product_content !!}
+                        </div>
+                    </li>
+                    @if($sections)
+                    @foreach($sections as $section)
+                        <li>
+                            <div class="uk-container ps-tab-header">
+                                <span>{{$section->product_section_name}}<span>
+                            </div>
+                            <div class="uk-container ps-tab-content uk-height-large">
+                                {!! $section->product_section_content !!}
+                            </div>
+                        </li>
                     @endforeach
                     @endif
                 </ul>
@@ -62,15 +83,18 @@
                         @endif
                     @endforeach
                     @endif
-                    @if($service->faqs) 
-                    <li class="nav-item">
-                        <a href="#{{'frequently-asked-questions'}}">Frequently asked questions</a>
-                    </li>
-                    @endif
                 </ul>
             </div>
             <div class="uk-width-expand@m">
                 <ul id="component-tab-left" class="uk-switcher">
+                    <li>
+                        <div class="uk-container ps-tab-header">
+                            <span>Overview<span>
+                        </div>
+                        <div class="uk-container ps-tab-content">
+                            {!! $product->product_content !!}
+                        </div>
+                    </li>
                     @if($sections)
                     @foreach($sections as $section)
                         <li>
@@ -81,87 +105,7 @@
                                 {!! $section->service_section_content !!}
                             </div>
                         </li>
-                        @if($loop->first)
-                            @if($service->service_product_show === 1) 
-                            <li>
-                                <div class="uk-container ps-tab-header">
-                                    <span>Mandatory Product List<span>
-                                </div>
-                                <div class="uk-container ps-tab-content">
-                                    <table id="mandatory-list" class="uk-table uk-table-hover uk-table-striped uk-table-small" style="width:100%">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Product Name</th>
-                                                <th class="uk-hidden">category</th>
-                                                @if($service->service_compliance)
-                                                @foreach(explode(',',$service->service_compliance) as $compliance)
-                                                @if($compliance)
-                                                <th>{{$compliance}}</th>
-                                                @endif
-                                                @endforeach
-                                                @endif
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @php
-                                                $category = ''
-                                            @endphp
-                                            @foreach($products as $product)
-                                            @if($category === $product->product_category_name)
-                                            <tr>
-                                                <td>{{$loop->iteration}}</td>
-                                                <td class="uk-hidden">{{$product->product_category_name}}</td>
-                                                <td>{{$product->product_name}}</td>
-                                                @if($service->service_compliance)
-                                                @foreach(unserialize($product->product_compliance) as $compliance)
-                                                    <td>{{$compliance}}</td>
-                                                @endforeach
-                                                @endif
-                                            </tr>
-                                            @else
-                                                <tr>
-                                                    <td class="uk-hidden"></td>
-                                                    <td colspan="100%" class="uk-text-center uk-text-bold">{{$product->product_category_name}}</td>
-                                                    <td class="uk-hidden"></td>
-                                                    @if($service->service_compliance)
-                                                    @foreach(unserialize($product->product_compliance) as $compliance)
-                                                        <td class="uk-hidden"></td>
-                                                    @endforeach
-                                                    @endif
-                                                </tr>
-                                                <tr class="product-page-link" data-slug="{{$product->product_slug}}">
-                                                    <td>{{$loop->iteration}}</td>
-                                                    <td class="uk-hidden">{{$product->product_category_name}}</td>
-                                                    <td>{{$product->product_name}}</td>
-                                                    @if($service->service_compliance)
-                                                    @foreach(unserialize($product->product_compliance) as $compliance)
-                                                        <td>{{$compliance}}</td>
-                                                    @endforeach
-                                                    @endif
-                                                </tr>
-                                            @endif
-                                            @php
-                                            $category = $product->product_category_name;
-                                            @endphp
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </li>
-                            @endif
-                        @endif
                     @endforeach
-                    @endif
-                    @if($service->faqs) 
-                    <li>
-                        <div class="section-content-header px-2">
-                            <span>{{$section->service_section_name}}<span>
-                        </div>
-                        <div class="px-2 pt-4">
-                        {!! $section->service_section_content !!}
-                        </div>
-                    </li>
                     @endif
                 </ul>
             </div>
