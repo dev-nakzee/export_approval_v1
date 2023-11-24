9679d7e9989910c50dae03a9a8780316dd8ad7a9
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-11-24 15:46:23 +0530

    Update product_details.blade.php

--- resources/views/frontend/pages/product_details.blade.php
+++ resources/views/frontend/pages/product_details.blade.php
@@ -16,25 +16,27 @@
         </ul>
     </div>
 </section>
-<section class="uk-section uk-padding-remove">
+<section class="uk-section uk-padding uk-padding-remove-vertical">
     <div class="uk-container">
-        <div uk-grid>
+        <div uk-grid id="js-oversized">
             <div class="uk-width-1-4@m">
-                <span class="uk-heading-bullet uk-text-bold">{{$product->product_name}}</span>
-                <ul class="uk-nav-default uk-nav-divider uk-margin-top" uk-nav>
-                    @if($sections)
-                    @foreach($sections as $section)
+                <div class="ps-details-section">
+                    <span class="uk-heading-bullet uk-text-bold">{{$product->product_name}}</span>
+                    <ul class="uk-nav-default uk-nav-divider uk-margin-top" uk-nav>
+                        @if($sections)
+                        @foreach($sections as $section)
+                            <li>
+                                <a href="#{{$section->product_section_slug}}">{{$section->product_section_name}}</a>
+                            </li>
+                        @endforeach
+                        @endif
                         <li>
-                            <a href="#{{$section->product_section_slug}}">{{$section->product_section_name}}</a>
+                            <a href="#">{{'Download Brochure'}}</a>
                         </li>
-                    @endforeach
-                    @endif
-                    <li>
-                        <a href="#">{{'Download Brochure'}}</a>
-                    </li>
-                </ul>
+                    </ul>
+                </div>
             </div>
-            <div class="uk-width-expand@m">
+            <div class="uk-width-3-4@m">
                 <div class="uk-container">
                     <div class="uk-container">
                         <span>Overview<span>
@@ -44,6 +46,18 @@
                         {!! $product->product_content !!}
                     </div>
                 </div>
+                @if($sections)
+                @foreach($sections as $section)
+                    <div class="uk-container">
+                        <div class="uk-container ps-tab-header">
+                            <span>{{$section->product_section_name}}<span>
+                        </div>
+                        <div class="uk-container ps-tab-content">
+                            {!! $section->product_section_content !!}
+                        </div>
+                    </div>
+                @endforeach
+                @endif
                 {{-- <ul id="component-tab-left" class="uk-switcher">
                     <li>
                         <div class="uk-container ps-tab-header">
