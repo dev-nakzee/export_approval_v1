796424e2ff22f7abbbdf94c09e427ed7e6c60b06
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-01 11:53:46 +0530

    Update home.blade.php

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -100,7 +100,7 @@
         </div>
         <div class="uk-card uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
             <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
-                <img src="images/light.jpg" alt="" uk-cover>
+                <img src="{{$sections[2]->media_path}} " alt="" uk-cover>
                 <canvas width="600" height="400"></canvas>
             </div>
             <div>
