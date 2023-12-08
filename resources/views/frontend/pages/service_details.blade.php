fbd5e4e2229d8f6a7eabf3dca2552cb34ddf379c
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-08 14:40:30 +0530

    commit

--- resources/views/frontend/pages/service_details.blade.php
+++ resources/views/frontend/pages/service_details.blade.php
@@ -44,31 +44,33 @@
                         </li>
                         @endif
                     </ul>
-                    <a class="uk-button uk-margin-top download-brochure-btn uk-padding-remove" href="#download-brochure"><svg fill="#052faa" height="800px" width="800px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 512 512">
-  <g>
-    <path d="m100.3,294.1l155.7,155.9 155.7-155.8h-69.3c-11.2,0-20.3-9.1-20.3-20.3v-222.2h-137.3v222.1c0,11.2-9.1,20.3-20.3,20.3h-64.2zm141.3,199l-204.6-204.9c-5.8-5.8-7.5-14.6-4.4-22.2 3.1-7.6 10.5-12.6 18.8-12.6h92.9v-222.1c0-11.2 9.1-20.3 20.3-20.3h177.9c11.2,0 20.3,9.1 20.3,20.3v222.1h98c8.2,0 15.6,5 18.8,12.6 3.1,7.6 1.4,16.3-4.4,22.2l-204.8,204.9c-10.5,10.4-18.2,10.6-28.8,0z"/>
-  </g>
-</svg> {{'Download Brochure'}}</a>
+                    <a class="uk-button uk-margin-top download-brochure-btn uk-padding-remove" href="#download-brochure">
+                            <svg fill="#052faa" height="800px" width="800px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 512 512">
+                            <g>
+                                <path d="m100.3,294.1l155.7,155.9 155.7-155.8h-69.3c-11.2,0-20.3-9.1-20.3-20.3v-222.2h-137.3v222.1c0,11.2-9.1,20.3-20.3,20.3h-64.2zm141.3,199l-204.6-204.9c-5.8-5.8-7.5-14.6-4.4-22.2 3.1-7.6 10.5-12.6 18.8-12.6h92.9v-222.1c0-11.2 9.1-20.3 20.3-20.3h177.9c11.2,0 20.3,9.1 20.3,20.3v222.1h98c8.2,0 15.6,5 18.8,12.6 3.1,7.6 1.4,16.3-4.4,22.2l-204.8,204.9c-10.5,10.4-18.2,10.6-28.8,0z"/>
+                            </g>
+                            </svg> {{'Download Brochure'}}
+                    </a>
                 </div>
             </div>
             <div class="uk-width-3-4@m uk-padding-remove-right">
                 @if($sections)
                 @foreach($sections as $section)
                     <div class="ps-sections" @if(!$loop->first) id="{{$section->service_section_slug}}" @endif>
-                        <div class="uk-container ps-tab-header uk-margin-remove-left uk-margin-remove-right">
+                        <div class="uk-section ps-tab-header uk-margin-remove-left uk-margin-remove-right">
                             <span>{{$section->service_section_name}}<span>
                         </div>
-                        <div class="uk-container ps-tab-content">
+                        <div class="uk-section ps-tab-content uk-margin-remove-left uk-margin-remove-right">
                             {!! $section->service_section_content !!}
                         </div>
                     </div>
                     @if($loop->first)
                         @if($service->service_product_show === 1) 
-                        <div class="ps-sections" id="mandatory-product-list">
-                            <div class="uk-container ps-tab-header">
+                        {{-- <div class="ps-sections" id="mandatory-product-list"> --}}
+                            <div class="uk-section ps-tab-header uk-margin-remove-left uk-margin-remove-right">
                                 <span>Product list for mandatory compliance<span>
                             </div>
-                            <div class="uk-container ps-tab-content">
+                            <div class="uk-section ps-tab-content uk-margin-remove-left uk-margin-remove-right">
                                 <table id="mandatory-list" class="uk-table uk-table-hover uk-table-striped uk-table-small" style="width:100%">
                                     <thead>
                                         <tr>
@@ -100,14 +102,26 @@
                                     </tbody>
                                 </table>
                             </div>
-                        </li>
+                        {{-- </div> --}}
                         @endif
                     @endif
                 @endforeach
                 @endif
                 @if($service->faqs) 
-                <div class="ps-sections" id="frequently-asked-questions">
-                   
+                <div class="uk-section ps-tab-header uk-margin-remove-left uk-margin-remove-right">
+                    <span>{{'Frequently Asked Questions'}}<span>
+                </div>
+                <div class="uk-section ps-tab-content uk-margin-remove-left uk-margin-remove-right">
+                    <ul uk-accordion class="uk-margin-bottom">
+                        @foreach(json_decode($service->faqs, true) as $que=>$ans)
+                        <li @if ($loop->first) class="uk-open" @endif>
+                            <a class="uk-accordion-title" href>{{$que}}</a>
+                            <div class="uk-accordion-content">
+                                {{ $ans }}
+                            </div>
+                        </li>
+                        @endforeach
+                    </ul>
                 </div>
                 @endif
             </div>
