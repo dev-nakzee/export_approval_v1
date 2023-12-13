b44e37731c0397f779127b3c42e93cf9bb0e6840
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-13 17:04:38 +0530

    Create holiday-list.blade.php

---
+++ resources/views/frontend/pages/holiday-list.blade.php
@@ -0,0 +1,46 @@
+@extends('frontend.layouts.master', ['pages' => 'Home'])
+@section('seo')
+<title>"title"</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+<meta property="og:title" content="" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="" />
+<meta property="og:url" content="{{env('APP_URL')}}" />
+<meta property="og:site_name" content="" />
+<meta property="og:image" content="" />
+<meta name="format-detection" content="telephone=no" />
+
+@endsection
+@section('content')
+<section class="uk-section page-header uk-padding-large uk-padding-remove-vertical">
+    <div class="uk-container uk-text-center">
+        <h1 class="uk-padding-small">
+            {{'Contact Us'}}
+        </h1>
+    </div>
+</section>
+<section class="uk-section uk-padding-large uk-padding-remove-vertical">
+    <div class="uk-padding-small">
+        <ul class="uk-breadcrumb uk-align-right uk-margin-remove-bottom">
+            <li><a href="{{route('frontend.site.home')}}">Home</a></li>
+            <li><span>{{'Contact-us'}}</span></li>
+        </ul>
+    </div>
+</section>
+<section class="uk-section home-section-3 uk-padding uk-padding-remove-top" style="">
+    <div class="section-two-heading uk-text-center uk-padding uk-padding-remove-vertical">
+        <p class="section-heading uk-margin-remove uk-padding-remove-vertical" style="color: #8a8a8a">
+            {{$sections[0]->section_name}} 
+        </p>
+        <span class="section-tagline">{!!$sections[0]->section_tagline!!}</span>
+        <p class="section-description">{!! $sections[0]->section_description !!}</p>
+    </div>
+</section>
+
+@endsection
+@section('scripts')
+
+@endsection
