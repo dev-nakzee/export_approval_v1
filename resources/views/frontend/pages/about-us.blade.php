66d60e2f38b14c321d8999f24f29f04ea75d5446
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-13 11:30:44 +0530

    Update about-us.blade.php

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -69,43 +69,73 @@
                                     <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
                                         <li>
                                             <img src="images/slider1.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>1</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Rajesh Kumar</h3>
+                                                <p class="uk-margin-remove">Founder - Director</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider2.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>2</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Amit Kumar</h3>
+                                                <p class="uk-margin-remove">Sales</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider3.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>3</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Sanjay Kapri</h3>
+                                                <p class="uk-margin-remove">Tech</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider4.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>4</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Sneh Lata</h3>
+                                                <p class="uk-margin-remove">HR</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider5.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>5</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Prince Kumar</h3>
+                                                <p class="uk-margin-remove">Sales</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider1.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>6</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Anuj Kumar</h3>
+                                                <p class="uk-margin-remove">Sales</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider2.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>7</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Nisha</h3>
+                                                <p class="uk-margin-remove">Sales</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider3.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>8</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Rinku</h3>
+                                                <p class="uk-margin-remove">Sales</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider4.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>9</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Kiran</h3>
+                                                <p class="uk-margin-remove">Sales</p>
+                                            </div>
                                         </li>
                                         <li>
                                             <img src="images/slider5.jpg" width="400" height="600" alt="">
-                                            <div class="uk-position-center uk-panel"><h1>10</h1></div>
+                                            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                                <h3 class="uk-margin-remove">Kajal</h3>
+                                                <p class="uk-margin-remove">Accounts</p>
+                                            </div>
                                         </li>
                                     </ul>
                                 </div>
