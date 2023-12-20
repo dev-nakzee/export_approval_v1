abac3b2600c7d191490ef6908670c3064aab24a3
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-20 13:02:17 +0530

    Home mandatory list section

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -17,7 +17,7 @@
 
     @section('content')
     @if ($agent->isMobile())
-    <section class="uk-section uk-padding-small">
+    <section class="uk-section uk-padding-small" style="background-color: {{$sections[0]->section_color}}">
         <span style="text-align: center !important;">
         {!! $sections[0]->section_content !!}
         </span>
@@ -54,7 +54,40 @@
             <div data-src="{{asset('frontend/images/indiamap.png')}}" uk-img class="uk-background-contain uk-padding-remove home-banner-image">
             </div>
             <div class="uk-width-1-1 uk-padding-remove">{!! $sections[0]->section_tagline !!}</div>
-
+    </section>
+    <section class="uk-section home-section-2 uk-padding-small">
+        <div class="uk-text-center">
+            <p class="section-heading uk-margin-remove-bottom">
+                {{$sections[1]->section_name}} 
+            </p>
+            <span class="section-tagline">{!!$sections[1]->section_tagline!!}</span>
+            <p class="section-description">{!! $sections[1]->section_description !!}</p>
+        </div>
+        <div class="uk-section uk-padding-small uk-padding-remove-top uk-padding-remove-right uk-margin-large-bottom">
+            <div class="uk-child-width-1-4@m uk-grid-match uk-flex-center" uk-grid>
+                @if($services)
+                @foreach($services as $service)
+                <div class="uk-margin-small-bottom home-ml-section">
+                    <article class="uk-comment uk-padding-remove uk-card uk-box-shadow-large uk-card-body uk-border-rounded" role="comment">
+                        <header class="uk-comment-header">
+                            <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                                <div class="uk-width-1-3">
+                                    <img class="uk-comment-avatar" src="{{$service->media_path}}" alt="{{$service->img_alt}}">
+                                </div>
+                                <div class="uk-width-expand uk-padding-small uk-padding-remove-left">
+                                    <a class="uk-link-reset" href="{{route('frontend.site.service', $service->service_slug)}}#mandatory-product-list"><h4 class="uk-comment-title uk-margin-remove">{{$service->service_name}}</h4>
+                                    <span>{!!$service->service_description!!}</span>
+                                    <p class="uk-margin-remove-vertical uk-padding-remove uk-text-small view-all-products">View All Products</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </header>
+                    </article>
+                </div>
+                @endforeach
+                @endif
+            </div>
+        </div>
     </section>
     @endsection
     @else
