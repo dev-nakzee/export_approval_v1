15d304a2ba0343b6e79075d445bba80bdf939a60
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-21 10:59:49 +0530

    Media coverage mobile responsive

--- resources/views/frontend/pages/media_coverage.blade.php
+++ resources/views/frontend/pages/media_coverage.blade.php
@@ -12,9 +12,40 @@
 <meta property="og:site_name" content="" />
 <meta property="og:image" content="" />
 <meta name="format-detection" content="telephone=no" />
-
 @endsection
 @section('content')
+@if($agent->isMobile())
+<section class="uk-section page-header uk-padding-small uk-padding-remove-vertical" uk-sticky="offset: 80">
+    <div class="uk-text-center">
+        <div>
+            <img class="uk-margin-remove uk-border-circle mobile-page-image" src="{{asset('frontend/images/microphone.png')}}" alt="">
+            <h2 class="uk-text-middle uk-inline uk-margin-remove">
+                <span class="uk-text-small" style="color: #8b8b8b;">{{'NEED CONTENT'}}</span>
+                <br> {{'Media Coverage'}}
+            </h2>
+        </div>
+    </div>
+</section>
+<section class="uk-section uk-padding-small uk-padding-remove-vertical">
+    <div class="uk-padding-small">
+        <ul class="uk-breadcrumb uk-text-center uk-margin-remove">
+            <li><a href="{{route('frontend.site.home')}}">Home</a></li>
+            <li><span>{{'Media Coverage'}}</span></li>
+        </ul>
+    </div>
+</section>
+<section class="uk-section uk-padding-small">
+    <div uk-grid class="uk-child-width-1-5@m uk-grid uk-container uk-width-1-1 uk-padding uk-padding-remove-right uk-padding-remove-vertical">
+    @if($news)
+    @foreach($news as $news)
+    <a href="{{$news->news_url}}" class="media-blocks" target="blank">
+        <img class="" src="{{$news->media_path}}" uk-img />
+    </a>
+    @endforeach
+    @endif
+    </div>
+</section>
+@else
 <section class="uk-section page-header uk-padding-large uk-padding-remove-vertical">
     <div class="uk-container uk-text-center">
         <h1 class="uk-padding-small">
@@ -43,6 +74,7 @@
         </div>
     </div>
 </section>
+@endif
 @endsection
 @section('scripts')
 
