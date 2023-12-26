64da091bebde46050673c3d6d5fe88cfa1d91b2e
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-26 11:48:35 +0530

    Commit

--- resources/views/frontend/pages/service_details.blade.php
+++ resources/views/frontend/pages/service_details.blade.php
@@ -21,15 +21,11 @@
     <div class="uk-container uk-text-center">
         <div>
             <img class="uk-margin-right uk-border-circle mobile-page-image" src="{{$service->media_path}}" alt="{{$service->img_alt}}">
-            <h2 class="uk-text-middle uk-inline uk-margin-remove">
-                <span class="uk-text-small" style="color: #8b8b8b;">{!!$service->service_description!!} For</span>
+            <h2 class="uk-text-middle uk-inline uk-margin-remove service-heading">
+                <span class="uk-text-small" style="color: #8b8b8b;">{!!$service->service_description!!}</span>
                 <br> {{$service->service_name}}
             </h2>
         </div>
-        <h2>
-            <img class="uk-margin-right uk-border-circle mobile-page-image" src="{{$service->media_path}}" alt="{{$service->img_alt}}">
-            {{$service->service_name}}
-        </h2>
     </div>
 </section>
 <section class="uk-section uk-padding-large uk-padding-remove-vertical">
@@ -111,10 +107,13 @@
 @else
 <section class="uk-section page-header uk-padding-large uk-padding-remove-vertical">
     <div class="uk-container uk-text-center">
-        <h1>
+        <div>
             <img class="uk-margin-right uk-border-circle service-details-image" src="{{$service->media_path}}" alt="{{$service->img_alt}}">
-            {{$service->service_name}}
-        </h1>
+            <h2 class="uk-text-middle uk-inline uk-margin-remove service-heading">
+                <span class="uk-text-small" style="color: #8b8b8b;">{!!$service->service_description!!}</span>
+                <br>{{$service->service_name}}
+            </h2>
+        </div>
     </div>
 </section>
 <section class="uk-section uk-padding-large uk-padding-remove-vertical">
