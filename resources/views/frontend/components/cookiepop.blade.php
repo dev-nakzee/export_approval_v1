465c923cc19c72b9c7f8ed09f5dcf08cf7ed786e
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-28 18:25:21 +0530

    comit

--- resources/views/frontend/components/cookiepop.blade.php
+++ resources/views/frontend/components/cookiepop.blade.php
@@ -2,7 +2,7 @@
     <div uk-grid>
             <div class="uk-width-4-5@m"><span class="uk-text-small uk-text-bold cookie-pop-div">We value your privacy</span>
             <p class="uk-small">We use cookies and tracking technologies to enhance your website experience. Personal data like IP address and browsing information may be processed for personalized ads, content measurement, and services development.</p>
-            <p class="uk-small">We prioritize transparency and security in your browsing experience, and you can manage consent preferences anytime. experience.</p>
+            <p class="uk-small">We prioritize transparency and security in your browsing experience, and you can manage consent preferences anytime.</p>
         </div>
         <div class="uk-width-1-5@m uk-text-middle cookie-pop-div">
             <button onclick="acceptAll()" class="uk-button uk-button-default">Accept All</button>
--- resources/views/frontend/components/popup.blade.php
+++ resources/views/frontend/components/popup.blade.php
@@ -1,9 +1,22 @@
 
 
-<div id="site-pop-up" uk-modal="bg-close: false; esc-close: false;">
-    <div class="uk-modal-dialog uk-modal-body">
+<div id="site-pop-up" class="uk-modal-container" uk-modal="bg-close: false; esc-close: false;">
+    <div class="uk-modal-dialog uk-modal-body uk-text-center">
         <button onclick="popupclose()" class="uk-modal-close-default" type="button" uk-close></button>
-        <img src="{{ asset('frontend/images/boss-bday.png') }}" />
+        <div class="uk-child-width-1-2" uk-grid>
+            <div class="uk-card-media-left uk-padding-large uk-padding-remove-horizontal" style="border-right: 1px solid #c9c9c9!important;">
+                <h4 class="uk-h1 uk-text-bold site-blue">Welcome</h4>
+                <h4 class="uk-h1 uk-text-bold site-blue">to</h4>
+                <img class="uk-margin-top" src="{{asset('frontend/images/logo.png')}}">
+            </div>
+            <div>
+                <div class="uk-card-body">
+                <p class="uk-h3 uk-text-bold site-orange">Enabling your Products for Export to India</p>
+                <p class="uk-h2 uk-text-bold site-blue uk-margin-large-top">Explore to obtain<br>all required certifications<br> for Indian Market</p>
+                <button onclick="popupclose()" class="uk-text-bold uk-button partner-form-btn uk-border-rounded uk-modal-close uk-margin-top" type="button">Now explore this website</button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <script>
