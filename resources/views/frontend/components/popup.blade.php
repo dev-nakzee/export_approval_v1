de7cbee69fdb7a2e654bf78d0bfa0205c69b922d
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-12-29 17:46:57 +0530

    commit

--- resources/views/frontend/components/popup.blade.php
+++ resources/views/frontend/components/popup.blade.php
@@ -1,13 +1,11 @@
-
-
 <div id="site-pop-up" class="uk-modal-container" uk-modal="bg-close: false; esc-close: false;">
     <div class="uk-modal-dialog uk-modal-body uk-text-center">
         <button onclick="popupclose()" class="uk-modal-close-default" type="button" uk-close></button>
         <div class="uk-child-width-1-2" uk-grid>
             <div class="uk-card-media-left uk-padding-large uk-padding-remove-horizontal" style="border-right: 1px solid #c9c9c9!important;">
-                <h4 class="uk-h1 uk-text-bold site-blue">Welcome</h4>
-                <h4 class="uk-h1 uk-text-bold site-blue">to</h4>
-                <img class="uk-margin-top" src="{{asset('frontend/images/logo.png')}}">
+                <h4 class="uk-h2 uk-text-bold site-blue">Welcome</h4>
+                <h4 class="uk-h2 uk-text-bold site-blue">to</h4>
+                <img class="uk-margin-top" style="height: 70px !important;" src="{{asset('frontend/images/logo.png')}}">
             </div>
             <div>
                 <div class="uk-card-body">
