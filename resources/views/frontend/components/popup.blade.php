f4813b97e6cba0140b8aab5f071b6b0424f21cdf
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-01-02 18:36:01 +0530

    Update popup.blade.php

--- resources/views/frontend/components/popup.blade.php
+++ resources/views/frontend/components/popup.blade.php
@@ -3,10 +3,10 @@
         <button onclick="popupclose()" class="uk-modal-close-default" type="button" uk-close></button>
         <div class="uk-child-width-1-1">
             <div class="uk-padding@m uk-padding-small@s uk-text-center">
-                <h4 class="uk-h2 uk-text-bold site-blue uk-margin-remove">Welcome to <span class="site-blue">Export</span><span class="site-orange">Approval</span></h4>
+                <h4 class="uk-h2 uk-text-bold site-blue uk-margin-remove">Welcome to <span class="site-blue">Export</span><span class="site-orange">Approval</span> !</h4>
                 <!-- <img class="uk-margin-large uk-margin-remove-top uk-margin-remove-horizontal" style="height: 60px !important;" src="{{asset('frontend/images/logo.png')}}"> -->
                 <!-- <h4 class="uk-h3 uk-text-bold site-blue">This website is about<br>Regulatory Product Compliance<br>for Indian Market</h4> -->
-                <p class="uk-h4 uk-text-normal uk-text-justify">Dear Visitor,<br><br>You will find <span class="site-blue uk-text-bolder">Regulatory Product Compliance</span> solution here, <span class="site-orange uk-text-bolder">how to enable your product for Export to India?</span><br><br>We are looking forword to assist you further.</p>
+                <p class="uk-h4 uk-text-normal uk-text-justify">Dear Visitor,<br><br>You will find Regulatory Product Compliance solution here, <span class="site-blue uk-text-bolder">how to enable your product for Export to India?</span><br><br>We are looking forword to assist you further.</p>
                 <button onclick="popupclose()" class="uk-text-bold uk-button partner-form-btn uk-border-rounded uk-text-center uk-modal-close uk-margin-large-top" type="button">Explore Now</button>
             </div>
         </div>
