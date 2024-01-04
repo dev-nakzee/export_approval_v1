a9f214b5c08bf8d81579aac0da823bb69eff8484
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-01-04 16:44:43 +0530

    Update contact.blade.php

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -81,7 +81,10 @@
 </form>
 <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
     <div class="uk-padding-remove-vertical uk-border-rounded">
-        <div style="overflow:hidden;max-width:100%;width:100%;height:100%;"><div id="google-maps-display" style="height:500%; width:100%;max-width:100%;"><iframe style="height:500%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Brand+Liaison+India+Private+Limited,+Veer+Savarkar+Block,+Guru+Nanak+Pura,+Laxmi+Nagar,+Delhi,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="google-map-html" href="https://www.bootstrapskins.com/themes" id="enable-map-info">premium bootstrap themes</a><style>#google-maps-display .text-marker{}.map-generator{max-width: 100%; max-height: 100%; background: none;}</style></div>
+        <div style="overflow:hidden;max-width:100%;width:100%;height:100%;">
+        <div id="google-maps-display" style="height:500%; width:100%;max-width:100%;">
+        <iframe style="height:500%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Brand+Liaison+India+Private+Limited,+Veer+Savarkar+Block,+Guru+Nanak+Pura,+Laxmi+Nagar,+Delhi,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div>
+            <style>#google-maps-display .text-marker{}.map-generator{max-width: 100%; max-height: 100%; background: none;}</style></div>
     </div>
 </div>
 <section class="uk-section">
