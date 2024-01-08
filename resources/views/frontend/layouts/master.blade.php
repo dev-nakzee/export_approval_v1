2e9cac248c33926fb6cadc381ac98a2750170d4a
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-01-08 12:37:57 +0530

    Update master.blade.php

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <link rel="icon" href="{{asset('frontend/images/favicon.png')}}http://example.com/favicon.png">
+        <link rel="icon" href="{{asset('frontend/images/favicon.ico')}}">
         @yield('seo')
         <meta name="audience" content="Everyone">
         <meta name="robots" content="index, follow">
