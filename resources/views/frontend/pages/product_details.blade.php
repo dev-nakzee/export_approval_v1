24bb63795d97a575d55947305e9614e53159b7f2
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-01-08 11:12:14 +0530

    Update product_details.blade.php

--- resources/views/frontend/pages/product_details.blade.php
+++ resources/views/frontend/pages/product_details.blade.php
@@ -14,6 +14,7 @@
 <meta name="format-detection" content="telephone=no" />
 @endsection
 @section('content')
+<h1 class="uk-hidden">{{$product->product_name}} - {{$service->service_name}} is Mandatory</h1>
 @if($agent->isMobile())
 <section class="uk-section page-header uk-padding-small" id="overview" uk-sticky="offset: 80;">
     <div class="uk-text-center">
